c864b6298a46401cf81b7bedc7189d2075ec8a6b
glorrymiss <113041245+glorrymiss@users.noreply.github.com>
2023-04-10 00:45:54 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -78,10 +78,7 @@ export class App extends Component {
   };
 
   hendleTakeSubmit = nameValue => {
-    this.setState({ nameValue });
-    // this.setState({ page: 1 });
-
-    this.setState({ images: [] });
+    this.setState({ nameValue, images: [] });
   };
 
   render() {
